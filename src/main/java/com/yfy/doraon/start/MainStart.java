9930fdd5fd011e396543933342bc243a8aa65ffb
前山顶 <qianshanding@163.com>
2019-03-19 10:49:52 +0800

    初始化项目

---
+++ src/main/java/com/yfy/doraon/start/MainStart.java
@@ -0,0 +1,11 @@
+package com.yfy.doraon.start;
+
+/**
+ * 启动类
+ * 打好包之后，直接nohup java -jar 包名.jar
+ */
+public class MainStart {
+    public static void main(String[] args) {
+        System.out.println("启动....");
+    }
+}
---
+++ src/test/com/yfy/doraon/java/test/App.java
@@ -0,0 +1,4 @@
+package com.yfy.doraon.test;
+
+public class App {
+}
